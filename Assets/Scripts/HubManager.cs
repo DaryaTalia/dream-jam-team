283371ecfdfae77d64c07d3b86dc76fe2a7fae96
Dylan Burke <dylan.burke@gmail.com>
2024-09-09 20:57:23 -0700

    basic singleton setup

---
+++ Assets/Scripts/HubManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubManager : MonoBehaviour
+{
+    void Start()
+    {
+        var spawnManager = GameManager.Instance.spawnManager;
+    }
+}
