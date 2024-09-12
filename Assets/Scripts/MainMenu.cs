a8468767e7fa29c532159379595103bc2d646006
GraphicEdit <contact@graphicedit.com>
2024-09-12 10:54:36 +0100

    MainMenu and Scene settings and  sample music

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject SettingsPanel;
+    public GameObject CreditsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Start()
+    {
+        SettingsPanel.SetActive(false);
+    }
+
+    public void OpenSettingsPanel()
+    {
+        SettingsPanel.SetActive(true);
+    }
+    public void CloseSettingsPanel()
+    {
+        SettingsPanel.SetActive(false);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
